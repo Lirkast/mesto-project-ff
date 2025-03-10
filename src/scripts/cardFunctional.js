3c1422a27204c1c510d96b51ced1f69a6a7ac913
Lir <arteom.galiullin@yandex.ru>
2025-03-10 11:45:20 +1000

    PART TWO!

--- src/scripts/cardFunctional.js
+++ src/scripts/cardFunctional.js
@@ -6,23 +6,23 @@ export function likeCard(likeButton) {
 
 export function cardCreation(name, link, deleteEl, likeEl, imageEl) {
 
-  const cardElement = cardTemplate.cloneNode(true).querySelector('.card');
-  const cardImage = cardElement.querySelector('.card__image');
-  const cardTitle = cardElement.querySelector('.card__title');
-  const cardLikeButton = cardElement.querySelector('.card__like-button');
-  const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+  const cardItem = cardTemplate.cloneNode(true).querySelector('.card');
+  const cardImage = cardItem.querySelector('.card__image');
+  const cardTitle = cardItem.querySelector('.card__title');
+  const cardLikeButton = cardItem.querySelector('.card__like-button');
+  const cardDeleteButton = cardItem.querySelector('.card__delete-button');
 
   cardTitle.textContent = name;
   cardImage.src = link;
   cardImage.alt = name;
 
-  cardDeleteButton.addEventListener('click', () => deleteEl(cardElement));
+  cardDeleteButton.addEventListener('click', () => deleteEl(cardItem));
   cardLikeButton.addEventListener('click', () => likeEl(cardLikeButton));
   cardImage.addEventListener('click', () => imageEl({ name, link }));
 
-  return cardElement;
+  return cardItem;
 }
 
-export function deleteItem(cardElement) {
-  cardElement.remove();
+export function deleteItem(cardItem) {
+  cardItem.remove();
 }
